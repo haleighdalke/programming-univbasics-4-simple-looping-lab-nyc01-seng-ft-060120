8bf20d9a9826e263c2aacfd0d6b2302acfce176f
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-07 21:31:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,6 +20,11 @@ def output_array(array)
   end
 end
 
-def return_string_array
-
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while array[count] do
+    new_array = array[count].to_s
+    count += 1
+  end
 end
