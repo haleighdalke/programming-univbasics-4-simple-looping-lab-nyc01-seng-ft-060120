bce1f0273d52b8d6c7bb00b2a03714f9ce19ac93
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-07 21:26:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,18 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times
+
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
